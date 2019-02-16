2cc0566394d5bb963b4e002ea49eabb726373400
nrebhun <nrebhun@apple.com>
2019-02-15 17:32:06 -0800

    Fix update script to handle multiple args

--- update.sh
+++ update.sh
@@ -13,11 +13,11 @@ function update_component() {
 if [ -z "$1" ]; then
     echo "No argument supplied. Try again."
 else
-    COMPONENTS_TO_UPDATE=$@
+    COMPONENTS_TO_UPDATE=( "$@" )
     if [ $@ == "all" ]; then
         COMPONENTS_TO_UPDATE=( "${ALL_COMPONENTS[@]}" )
     fi
-    
+
     update_component "${COMPONENTS_TO_UPDATE[@]}"
     echo "Configuration updated! Please reload!"
 fi
